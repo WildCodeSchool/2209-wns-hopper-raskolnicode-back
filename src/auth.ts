d10a8830d977fbaa15442b3939763b174faf7c11
Soufiane Aït <soufiane.ait@outlook.fr>
2023-01-27 14:31:09 +0100

    fixing auth

--- src/auth.ts
+++ src/auth.ts
@@ -7,30 +7,30 @@ import { IContext } from './resolvers/Users';
 export const customAuthChecker: AuthChecker<IContext> = async (
   { root, args, context, info }, roles,
 ) => {
-  console.log('executing auth checker')
+
   const token = context.token
-  console.log('TOKEN in auth', token)
 
   if (token === null || token === '') {
-    context.user = null
     return false
   }
 
   try {
-    const decodedToken: { userId: number } = jwtVerify(token, process.env.JWT_SECRET_KEY) as any
+    const decodedToken: { userId: number } = jwtVerify(
+      token, process.env.JWT_SECRET_KEY
+    ) as any
     const userId = decodedToken.userId
 
-    const user = await datasource.getRepository(User).findOne({ where: { id: userId } })
+    const user = await datasource.getRepository(User).findOne({
+      where: { id: userId }
+    })
 
     if (!user) {
       return false
     }
 
     context.user = user
-    console.log('context user', user)
     return true
   } catch {
-    context.user = null
     return false
   }
 
