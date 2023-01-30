569c9eaddfaedfbd090070c756d05dff8b8e3485
alan3700 <alandaunay1@gmail.com>
2023-01-30 15:31:17 +0100

    findOnePost

--- src/resolvers/Posts.ts
+++ src/resolvers/Posts.ts
@@ -1,4 +1,4 @@
-import { Resolver, Mutation, Arg, Query } from "type-graphql";
+import { Resolver, Mutation, Arg, Query,ID } from "type-graphql";
 import { User } from "../entities/User";
 import datasource from "../utils";
 import { Post, PostInput } from "../entities/Post";
@@ -15,5 +15,16 @@ export class PostsResolver {
   @Query(() => [Post])
   async posts(): Promise<Post[]> {
     return await datasource.getRepository(Post).find({});
+  }
+
+  
+  @Query(() => Post, { nullable: true })
+  async post(@Arg("PostId", () => ID) id: number): Promise<Post | null> {
+    const post = await datasource.getRepository(Post).findOne({ where: { id }})
+
+    if (post === null) {
+      throw new Error('Il n\'y a pas d\'article pour cette recherche')
+    }
+    return post
   }
 }
