f261a17c1e3317db92278dc6ddab74097e21e5d4
Vehementz <vale-tudo91@sfr.fr>
2023-05-25 16:28:15 +0200

    code cleaned

--- src/resolvers/Users.ts
+++ src/resolvers/Users.ts
@@ -113,27 +113,6 @@ export class UsersResolver {
 
   }
 
-
-
-
-  //   @Authorized()
-  //   @Query(() => User, { nullable: true })
-  //   async loggedUser(@Ctx() context: IContext): Promise<User | null> {
-
-  //   const userRepository = datasource.getRepository(User);
-
-  // const user = await userRepository.findOne({
-  //   where: { id: context.user.id },
-  //   relations: {blogs: {picture: true}}
-  // });
-
-  //   return user;
-
-
-
-
-
-  // @Authorized("SUPERADMIN")
   @Query(() => [User])
   async getUsers(): Promise<User[]> {
     return await datasource
